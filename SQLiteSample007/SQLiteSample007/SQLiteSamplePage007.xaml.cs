cf618d068236af59cfc425dd00395f5f2d90866b
謙太 可士村 <150365@jc-21.jp>
2017-11-22 14:17:57 +0900

    ボタン配置できるのか

--- SQLiteSample007/SQLiteSample007/SQLiteSamplePage007.xaml.cs
+++ SQLiteSample007/SQLiteSample007/SQLiteSamplePage007.xaml.cs
@@ -4,7 +4,7 @@ namespace SQLiteSample007
 {
     public partial class SQLiteSamplePage007 : ContentPage
     {
-        public SQLiteSamplePage007()
+        /*public SQLiteSamplePage007()
         {
             InitializeComponent();
 
@@ -26,6 +26,41 @@ namespace SQLiteSample007
             }
 
             Content = layout;
+        }*///おかしくなったら下のソース消してこれのコメントアウトを解呪する!
+
+        public SQLiteSamplePage007()
+        {
+            InitializeComponent();
+
+            var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
+
+            //Userテーブルに適当なデータを追加する
+            UserModel007.insertUser("鈴木");
+            UserModel007.insertUser("田中");
+            UserModel007.insertUser("斎藤");
+
+            var buttonAdd = new Button
+            {
+                WidthRequest = 60,
+                TextColor = Color.White,
+                Text = "Add"
+            };
+
+
+            //Userテーブルの行データを取得
+            var query = UserModel007.selectUser();
+
+            foreach (var user in query)
+            {
+
+                //Userテーブルの名前列をLabelに書き出す
+                layout.Children.Add(new Label { Text = user.Name });
+            }
+
+            Content = layout;
         }
+
+
+
     }
 }
